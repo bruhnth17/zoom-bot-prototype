a3fd6efe7809abe44334f1124c4f416f436f29d0
Thomas Bruhn <tom-95@hotmail.de>
2020-12-08 15:25:39 +0100

    minimal websocket example

---
+++ public/client.js
@@ -0,0 +1,20 @@
+var socket = io();
+
+// Incoming socket events
+socket.on('joke', e => {
+    const list = document.getElementById('list');
+    const jokeElement = document.createElement("p");
+    const newListItem = document.createElement("li");
+    jokeElement.innerText = e.text;
+    newListItem.appendChild(jokeElement);
+    console.log(newListItem);
+    list.appendChild(newListItem);
+});
+
+
+
+// Outgoing socket events
+const jokeOnClick = () => {
+    // This will emit the event to all connected sockets
+    socket.emit('joke', { text: "My mexican uncle is taking anti anxiety pills. It's for his panic attacks" });
+}
